c7dc1d1bd98ecd06f448b6b5a2a1f5618177db40
trakt <trakt@DESKTOP-UDGVRCB>
2023-11-19 19:27:21 +0300

    Добавьте файлы проекта.

---
+++ 27.11.2023/27.11.2023.cpp
@@ -0,0 +1,52 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <Windows.h>
+#include <iostream>
+#include <stdio.h>
+#include <string.h>
+
+using namespace std;
+
+void Task1(FILE* input_File, FILE* output_File)
+{
+    char line[256];
+    char prevLine[256];
+    if (fgets(line, sizeof(line), input_File)) {
+        while (fgets(prevLine, sizeof(prevLine), input_File)) {
+            fputs(line, output_File);
+            strncpy(line, prevLine, sizeof(line));
+        }
+    }
+    fclose(input_File);
+    fclose(output_File);
+}
+
+
+int main() 
+{
+    //Task 1
+    FILE* input_File = fopen("input.txt", "r");
+    FILE* output_File = fopen("output.txt", "w");
+    if (!input_File || !output_File) {
+        printf("Failed to open file.\n");
+        return 1;
+    }
+    Task1(input_File, output_File);
+
+    //Task 2
+    FILE* inFile = fopen("input2.txt", "r");
+    FILE* outFile = fopen("output2.txt", "w");
+    char line[2048];
+    int lineNumber = 1;
+
+    while (fgets(line, sizeof(line), inFile)) {
+        if (lineNumber % 2 != 0) {
+            fputs(line, outFile);
+        }
+        lineNumber++;
+    }
+
+    fclose(inFile);
+    fclose(outFile);
+
+    return 0;
+}
